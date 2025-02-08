369bceafc8952c6c8cf9f26342b4cd9375eddcae
Vadim Shikhovtsov <5208643@mail.ru>
2025-02-08 14:44:38 +0300

    day4

---
+++ Day_4/form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="form-handler.php" method="POST">
+        <input type="text" name="fname" placeholder="Имя">
+        <input type="submit" value="Отправить">
+    </form>
+</body>
+</html>
+https://github.com/denis-basov/HTML_CSS_18_01.git
